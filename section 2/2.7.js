c4188bcb14d82ce1382b50678bf5c5fa1515a507
Danil <bloodshoww@mail.ru>
2025-01-31 14:16:44 +0300

    section 2 ready

---
+++ section 2/2.7.js
@@ -0,0 +1,6 @@
+// Напишите программу, которая преобразует строку "123" в число, а число 456 в строку.
+
+// const num = Number('123')
+// const str = String(123)
+//
+// console.log(typeof num, typeof  str)
---
+++ section 2/2.9.js
@@ -0,0 +1,23 @@
+// Операторы сравнения
+
+// Задание 1
+// Операторы сравнения
+// важность: 5
+// Каким будет результат этих выражений?
+//
+// 5 > 4 // true
+// "ананас" > "яблоко" // false
+// "2" > "12" // true
+// undefined == null // true
+// undefined === null // false
+// null == "\n0\n" // false
+// null === +"\n0\n" // false
+
+// Решение
+// true
+// false
+// false
+// true
+// false
+// true
+// false
